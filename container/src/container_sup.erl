c01603e93fcd5dab77eb26d0855c7239a3bd0e66
Paolo Sberna <linbeaverx@yahoo.it>
2012-07-17 18:50:45 +0200

    First files added.

---
+++ container/src/container_sup.erl
@@ -0,0 +1,71 @@
+%%%-------------------------------------------------------------------
+%%% @author Paolo C Sberna <reliablebeaver86-cs@yahoo.it>
+%%% @copyright (C) 2012, Paolo C Sberna
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17 Jul 2012 by Paolo C Sberna <reliablebeaver86-cs@yahoo.it>
+%%%-------------------------------------------------------------------
+-module(container_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    RestartStrategy = one_for_one,
+    MaxRestarts = 1000,
+    MaxSecondsBetweenRestarts = 3600,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = permanent,
+    Shutdown = 2000,
+
+    ManagerChild = {manager, {manager, start_link, []},
+                    Restart, Shutdown, worker, [manager]},
+    AgentsChild = {agents, {agents_sup, start_link, []},
+                  Restart, Shutdown, supervisor, [agents_sup]},
+
+    {ok, {SupFlags, [ManagerChild, AgentsChild]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
