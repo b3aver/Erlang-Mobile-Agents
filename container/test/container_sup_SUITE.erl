f0fbf7098af503456d76b4b7580e2a6c0a7ee91a
Paolo Sberna <linbeaverx@yahoo.it>
2012-07-19 22:33:37 +0200

    Tests of container_sup.

---
+++ container/test/container_sup_SUITE.erl
@@ -0,0 +1,292 @@
+%%%-------------------------------------------------------------------
+%%% @author Paolo C Sberna <reliablebeaver86-cs@yahoo.it>
+%%% @copyright (C) 2012, Paolo C Sberna
+%%% @doc
+%%%
+%%% @end
+%%% Created : 19 Jul 2012 by Paolo C Sberna <reliablebeaver86-cs@yahoo.it>
+%%%-------------------------------------------------------------------
+-module(container_sup_SUITE).
+
+%% Note: This directive should only be used in test suites.
+-compile(export_all).
+
+-include_lib("common_test/include/ct.hrl").
+
+%%--------------------------------------------------------------------
+%% COMMON TEST CALLBACK FUNCTIONS
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% @doc
+%%  Returns list of tuples to set default properties
+%%  for the suite.
+%%
+%% Function: suite() -> Info
+%%
+%% Info = [tuple()]
+%%   List of key/value pairs.
+%%
+%% Note: The suite/0 function is only meant to be used to return
+%% default data values, not perform any other operations.
+%%
+%% @spec suite() -> Info
+%% @end
+%%--------------------------------------------------------------------
+suite() ->
+    [{timetrap,{minutes,10}}].
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Initialization before the whole suite
+%%
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for skipping the suite.
+%%
+%% Note: This function is free to add any key/value pairs to the Config
+%% variable, but should NOT alter/remove any existing entries.
+%%
+%% @spec init_per_suite(Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% @end
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Cleanup after the whole suite
+%%
+%% Config - [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%%
+%% @spec end_per_suite(Config) -> _
+%% @end
+%%--------------------------------------------------------------------
+end_per_suite(_Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Initialization before each test case group.
+%%
+%% GroupName = atom()
+%%   Name of the test case group that is about to run.
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding configuration data for the group.
+%% Reason = term()
+%%   The reason for skipping all test cases and subgroups in the group.
+%%
+%% @spec init_per_group(GroupName, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% @end
+%%--------------------------------------------------------------------
+init_per_group(_GroupName, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Cleanup after each test case group.
+%%
+%% GroupName = atom()
+%%   Name of the test case group that is finished.
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding configuration data for the group.
+%%
+%% @spec end_per_group(GroupName, Config0) ->
+%%               void() | {save_config,Config1}
+%% @end
+%%--------------------------------------------------------------------
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Initialization before each test case
+%%
+%% TestCase - atom()
+%%   Name of the test case that is about to be run.
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for skipping the test case.
+%%
+%% Note: This function is free to add any key/value pairs to the Config
+%% variable, but should NOT alter/remove any existing entries.
+%%
+%% @spec init_per_testcase(TestCase, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% @end
+%%--------------------------------------------------------------------
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Cleanup after each test case
+%%
+%% TestCase - atom()
+%%   Name of the test case that is finished.
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%%
+%% @spec end_per_testcase(TestCase, Config0) ->
+%%               void() | {save_config,Config1} | {fail,Reason}
+%% @end
+%%--------------------------------------------------------------------
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Returns a list of test case group definitions.
+%%
+%% Group = {GroupName,Properties,GroupsAndTestCases}
+%% GroupName = atom()
+%%   The name of the group.
+%% Properties = [parallel | sequence | Shuffle | {RepeatType,N}]
+%%   Group properties that may be combined.
+%% GroupsAndTestCases = [Group | {group,GroupName} | TestCase]
+%% TestCase = atom()
+%%   The name of a test case.
+%% Shuffle = shuffle | {shuffle,Seed}
+%%   To get cases executed in random order.
+%% Seed = {integer(),integer(),integer()}
+%% RepeatType = repeat | repeat_until_all_ok | repeat_until_all_fail |
+%%              repeat_until_any_ok | repeat_until_any_fail
+%%   To get execution of cases repeated.
+%% N = integer() | forever
+%%
+%% @spec: groups() -> [Group]
+%% @end
+%%--------------------------------------------------------------------
+groups() ->
+    [].
+
+%%--------------------------------------------------------------------
+%% @doc
+%%  Returns the list of groups and test cases that
+%%  are to be executed.
+%%
+%% GroupsAndTestCases = [{group,GroupName} | TestCase]
+%% GroupName = atom()
+%%   Name of a test case group.
+%% TestCase = atom()
+%%   Name of a test case.
+%% Reason = term()
+%%   The reason for skipping all groups and test cases.
+%%
+%% @spec all() -> GroupsAndTestCases | {skip,Reason}
+%% @end
+%%--------------------------------------------------------------------
+all() -> 
+    [init_case, start_stop_case].
+
+
+%%--------------------------------------------------------------------
+%% TEST CASES
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% @doc 
+%%  Test case info function - returns list of tuples to set
+%%  properties for the test case.
+%%
+%% Info = [tuple()]
+%%   List of key/value pairs.
+%%
+%% Note: This function is only meant to be used to return a list of
+%% values, not perform any other operations.
+%%
+%% @spec TestCase() -> Info 
+%% @end
+%%--------------------------------------------------------------------
+my_test_case() -> 
+    [].
+
+%%--------------------------------------------------------------------
+%% @doc Test case function. (The name of it must be specified in
+%%              the all/0 list or in a test case group for the test case
+%%              to be executed).
+%%
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for skipping the test case.
+%% Comment = term()
+%%   A comment about the test case that will be printed in the html log.
+%%
+%% @spec TestCase(Config0) ->
+%%           ok | exit() | {skip,Reason} | {comment,Comment} |
+%%           {save_config,Config1} | {skip_and_save,Reason,Config1}
+%% @end
+%%--------------------------------------------------------------------
+my_test_case(_Config) -> 
+    ok.
+
+
+%% ----------------
+%% White box tests
+%% ----------------
+init_case(_Config) ->
+    %% check init
+    RestartStrategy = one_for_one,
+    MaxRestarts = 1000,
+    MaxSecondsBetweenRestarts = 3600,
+    ExpectedSupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = permanent,
+    Shutdown = 2000,
+    ManagerChild = {manager, {manager, start_link, []},
+                    Restart, Shutdown, worker, [manager]},
+    AgentsChild = {agents, {agents_sup, start_link, []},
+                  Restart, Shutdown, supervisor, [agents_sup]},
+    ExpectedChildSpecList = [ManagerChild, AgentsChild],
+
+    {ok, {ExpectedSupFlags, ExpectedChildSpecList}} = container_sup:init([]),
+
+    %% check ChildSpec
+    ok = supervisor:check_childspecs(ExpectedChildSpecList),
+
+    ok.
+
+
+%% ----------------
+%% Black box tests
+%% ----------------
+start_stop_case(_Config) ->
+    process_flag(trap_exit, true),
+
+    undefined = whereis(container_sup),
+    undefined = whereis(manager),
+    undefined = whereis(agents),
+
+    %% check start
+    {ok, ContainerPid} = container_sup:start_link(),
+    receive after 100 -> nil end,
+    ContainerPid = whereis(container_sup),
+    %ct:log("~p ~p ~p ~p",[whereis(manager),whereis(agents),ContainerPid, self()]),
+    Children = supervisor:which_children(container_sup),
+    %ct:log("~p", [Children]),
+    {manager, ManagerPid, worker, _ModM} = lists:keyfind(manager, 1, Children),
+    {agents, AgentsPid, supervisor, _ModA} = lists:keyfind(agents, 1, Children),
+    true = erlang:is_process_alive(ManagerPid),
+    true = erlang:is_process_alive(AgentsPid),
+
+    %% check stop
+    exit(ContainerPid, shutdown),
+    
+    receive
+        {'EXIT', _Pid, _Reason} ->
+            false = erlang:is_process_alive(ContainerPid),
+            false = erlang:is_process_alive(ManagerPid),
+            false = erlang:is_process_alive(AgentsPid)
+    after
+        2000 -> exit(shutdown_error)
+    end,
+
+    ok.
+
+
