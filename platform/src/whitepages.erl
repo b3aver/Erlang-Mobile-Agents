7019e0a804167225e8dffd1bf63838a77ac938b8
Paolo Sberna <linbeaverx@yahoo.it>
2012-07-23 18:15:51 +0200

    First sketch of platform.

---
+++ platform/src/whitepages.erl
@@ -0,0 +1,147 @@
+%%%-------------------------------------------------------------------
+%%% @author Paolo C Sberna <reliablebeaver86-cs@yahoo.it>
+%%% @copyright (C) 2012, Paolo C Sberna
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23 Jul 2012 by Paolo C Sberna <reliablebeaver86-cs@yahoo.it>
+%%%-------------------------------------------------------------------
+-module(whitepages).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+-export([register/2]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-include("whitepages.hrl").
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+register(Container, Node) ->
+    gen_server:call(?SERVER, {register, Container, Node}).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    {ok, #state{containers=[]}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call({register, Container, Node}, _From, #state{containers=ContList}) ->
+    %% TODO check that From is the Container's pid
+    case lists:keysearch(Container, #container.name, ContList) of
+        {value, ContainerSaved} ->
+            %% Container is already present in the ContList
+            Reply = {error, already_present},
+            State = #state{containers=ContList},
+            {reply, Reply, State};
+        false ->
+            %% Container is not in the ContList
+            State = #state{containers=[#container{name=Container, node=Node}
+                                       |ContList]},
+            Reply = ok,
+            {reply, Reply, State}
+        end;
+
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
---
+++ platform/src/whitepages.hrl
@@ -0,0 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author Paolo C Sberna <reliablebeaver86-cs@yahoo.it>
+%%% @copyright (C) 2012, Paolo C Sberna
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23 Jul 2012 by Paolo C Sberna <reliablebeaver86-cs@yahoo.it>
+%%%-------------------------------------------------------------------
+
+
+-record(state, {containers}).
+-record(container, {name, node}).
