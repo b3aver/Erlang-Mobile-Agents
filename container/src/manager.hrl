885062775cec0bf2a5abfbfaca6cdb3028e34bf7
Paolo Sberna <linbeaverx@yahoo.it>
2012-07-23 18:14:51 +0200

    Added manager.hrl.

---
+++ container/src/manager.hrl
@@ -0,0 +1,16 @@
+%%%-------------------------------------------------------------------
+%%% @author Paolo C Sberna <reliablebeaver86-cs@yahoo.it>
+%%% @copyright (C) 2012, Paolo C Sberna
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23 Jul 2012 by Paolo C Sberna <reliablebeaver86-cs@yahoo.it>
+%%%-------------------------------------------------------------------
+
+
+
+
+-record(state, {agents}).
+
+-record(agent, {name, module, function, arguments, state}).
+
